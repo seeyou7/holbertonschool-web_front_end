dd570be8fcde5ac364ebb457a9bc95d9ebefdedc
seeyou7 <5764@holbertonstudents.com>
2024-09-10 13:27:10 +0200

    Timing ExEcution using prime number

--- Javascript_advanced/9-prime.js
+++ Javascript_advanced/9-prime.js
@@ -10,7 +10,7 @@ function isPrime(number){
 
 function countPrimeNumbers(){
     let count = 0;
-    for (let i = 2; i <= 200; i++){
+    for (let i = 2; i <= 100; i++){
         if(isPrime(i)){
             count++
         }
