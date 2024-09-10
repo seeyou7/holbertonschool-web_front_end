f4713d6a1228ce0be13f2cf75e86dd8f1d12a63c
seeyou7 <5764@holbertonstudents.com>
2024-09-10 13:31:27 +0200

    Execution stack & timing execution

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,27 @@
+function isPrime(number){
+    for (let i = 2; i <= Math.sqrt(number); i++){
+        if(number % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function countPrimeNumbers(){
+    let count = 0;
+    for (let i = 2; i <= 100; i++){
+        if(isPrime(i)){
+            count++
+        }
+    }
+    return count;
+
+}
+
+let startTime = performance.now();
+for (let i = 1; i <= 100; i++){
+    countPrimeNumbers();
+}
+let endTime = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${endTime - startTime} miliseconds.`);
